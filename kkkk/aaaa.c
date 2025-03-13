df9c344ae74a31052c13c691e0011eea117272e1
davidmoreira94 <davidmoreirafrota2312@gmail.com>
2025-03-13 01:38:47 +0000

    Supertrunfo

---
+++ kkkk/aaaa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+   int a = 10;
+   int b = 3;
+   int soma = a + b;
+   int diferenca = a - b;
+   int produto = a * b;
+   int quociente = a / b;
+
+   printf("Soma: %d/n",soma);
+   printf("diferenca: %d/n",diferenca);
+   printf("produto: %d/n",produto);
+   printf("quociente: %d/n",quociente);
+
+    return 0;
+
+}
